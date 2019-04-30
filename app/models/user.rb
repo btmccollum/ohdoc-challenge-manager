5d35b20d4644a99049eba2cdc2a31bf9ffbdac6d
btmccollum <btmcco8@gmail.com>
2019-04-30 16:09:49 -0500

    adding validations for user class

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,10 +4,15 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
+  # used to handle encryption upon persistance
   attr_encrypted_options.merge!(encode: true, encode_iv: true, encode_salt: true)
   attr_encrypted :encrypted_twitter_token, key: ENV["AEKEY"]
   attr_encrypted :encrypted_github_token_iv, key: ENV["AEKEY"]
   attr_encrypted :encrypted_twitter_token, key: ENV["AEKEY"]
   attr_encrypted :encrypted_github_token_iv, key: ENV["AEKEY"]
 
+  validates :username, presence: true, uniqueness: true
+  validates :email, presence: true, uniqueness: true
+  validates :password, presence: true, on: :create
+  validates :password, confirmation: true, on: :create
 end
