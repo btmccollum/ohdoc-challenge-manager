f6356b9495b6d9a70080ba978d22a91dc27af842
btmccollum <btmcco8@gmail.com>
2019-05-03 21:44:54 -0500

    outlining steps for github put request

--- app/controllers/submissions_controller.rb
+++ app/controllers/submissions_controller.rb
@@ -1,2 +1,50 @@
 class SubmissionController < ActionController::Base
+
+
+    # need to handle both twitter and/or github submissions when applicable
+
+    # GitHub:
+    # 1) # fetch blob SHA to be able to commit
+    # https://api.github.com/repos/btmccollum/100-days-of-code/contents/log.md
+
+    # 2) response, need to grab "sha" for patch, and contents"
+        # {
+        #     "name": "log.md",
+        #     "path": "log.md",
+        #     "sha": "eb0a318d9b41868e202e13c9b0f3f58c192834d5",
+        #     "size": 52988,
+        #     "url": "https://api.github.com/repos/btmccollum/100-days-of-code/contents/log.md?ref=master",
+        #     "html_url": "https://github.com/btmccollum/100-days-of-code/blob/master/log.md",
+        #     "git_url": "https://api.github.com/repos/btmccollum/100-days-of-code/git/blobs/eb0a318d9b41868e202e13c9b0f3f58c192834d5",
+        #     "download_url": "https://raw.githubusercontent.com/btmccollum/100-days-of-code/master/log.md",
+        #     "type": "file",
+        #     "content": "IyAxMDAgRGF5cyBPZiBDb2RlIC0gTG9nCgojIyMgRGF5IDE6IEphbnVhcnkg\nMiwgMjAxOSAKCioqVG9kYXkncyBQcm9ncm...M6Ly9naXRodWIuY29tL2J0bWNjb2xsdW0vZHJvcGxl\ndC1hcGkKCioqVGltZSBTcGVudCBUb2RheToqKiAxMCBob3VycwoKKipKb3Vy\nbmV5IFRpbWUqKiAxMDEwLjUgaG91cnM=\n",
+        #     "encoding": "base64",
+        #     "_links": {
+        #         "self": "https://api.github.com/repos/btmccollum/100-days-of-code/contents/log.md?ref=master",
+        #         "git": "https://api.github.com/repos/btmccollum/100-days-of-code/git/blobs/eb0a318d9b41868e202e13c9b0f3f58c192834d5",
+        #         "html": "https://github.com/btmccollum/100-days-of-code/blob/master/log.md"
+        #     }
+        # }
+
+    # # github submission
+
+    #3) # need to decode original contents from base 64, append the new log entry, and then encode to base 64
+
+    # 4) POST to https://api.github.com/repos/btmccollum/100-days-of-code/contents/log.md
+
+    # # working Body sample to complete a PUT request to modify file contents
+    # header = {
+    #     "Authorization": "token #{GITHUB_SECRET}",
+    #     "User-Agent": "ohdoc-challenge-manager/v1",
+    #     "Content-Type": "application/json"
+    # }
+
+    # body = {
+    #     "message": "testing API put method",
+    #     # content must be Base64 encoded
+    #     "content": "RGF5IDA6IEZlYnJ1YXJ5IDMwLCAyMDE2IChFeGFtcGxlIDEpIChkZWxldGUgbWUgb3IgY29tbWVudCBtZSBvdXQpIFRvZGF5J3MgUHJvZ3Jlc3M6IEZpeGVkIENTUywgd29ya2VkIG9uIGNhbnZhcyBmdW5jdGlvbmFsaXR5IGZvciB0aGUgYXBwLiAgVGhvdWdodHM6IEkgcmVhbGx5IHN0cnVnZ2xlZCB3aXRoIENTUywgYnV0LCBvdmVyYWxsLCBJIGZlZWwgbGlrZSBJIGFtIHNsb3dseSBnZXR0aW5nIGJldHRlciBhdCBpdC4gQ2FudmFzIGlzIHN0aWxsIG5ldyBmb3IgbWUsIGJ1dCBJIG1hbmFnZWQgdG8gZmlndXJlIG91dCBzb21lIGJhc2ljIGZ1bmN0aW9uYWxpdHkuICBMaW5rIHRvIHdvcms6IENhbGN1bGF0b3IgQXBw",
+    #     "sha": "eb0a318d9b41868e202e13c9b0f3f58c192834d5"
+    # }
 end
+
