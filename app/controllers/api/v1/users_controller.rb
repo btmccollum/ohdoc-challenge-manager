3bd17021d2c62dc48f4945baedd19e67de025aea
btmccollum <btmcco8@gmail.com>
2019-05-08 18:26:44 -0500

    adding show and adding error handling to user create action

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -1,16 +1,22 @@
 class Api::V1::UsersController < ApplicationController
-    def create
-        user = User.new(user_params)
+  def show
+    render json: { user: current_user }, status: :ok
+  end
 
-        if user.save
-            jwt = Auth.encrypt({id: user.id})
-            render json: { user: user, jwt: jwt }, status: :ok
-        end
+  def create
+    user = User.new(user_params)
+
+    if user.save
+      jwt = Auth.encrypt({id: user.id})
+      render json: { user: user, jwt: jwt }, status: :ok
+    else
+      render json: { errors: user.errors}, status: 422    
     end
+  end
 
-    private
+  private
 
-    def user_params
-        params.require(:user).permit(:username, :email, :password, :twitter_token, :github_token)
-    end
+  def user_params
+    params.require(:user).permit(:username, :email, :password, :twitter_token, :github_token)
+  end
 end
