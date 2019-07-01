d5f112aaa54f92c41df787c74f42820a3ef1d6d6
btmccollum <btmcco8@gmail.com>
2019-06-30 21:26:50 -0500

    fixed issue with API tests with VCR

--- spec/mailers/previews/user_mailer_preview.rb
+++ spec/mailers/previews/user_mailer_preview.rb
@@ -1,9 +1,9 @@
 # Preview all emails at http://localhost:3000/rails/mailers/user_mailer
-class UserMailerPreview < ActionMailer::Preview
+# class UserMailerPreview < ActionMailer::Preview
 
-  # Preview this email at http://localhost:3000/rails/mailers/user_mailer/password_reset
-  def password_reset
-    UserMailer.password_reset
-  end
+#   # Preview this email at http://localhost:3000/rails/mailers/user_mailer/password_reset
+#   def password_reset
+#     UserMailer.password_reset
+#   end
 
-end
+# end
