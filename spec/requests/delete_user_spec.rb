a0f15d940c16a918b8602a76aefe016e03014ceb
btmccollum <btmcco8@gmail.com>
2019-08-07 15:46:21 -0500

    fixing bug with delete test and debugging travis ci implementation

--- spec/requests/delete_user_spec.rb
+++ spec/requests/delete_user_spec.rb
@@ -2,7 +2,7 @@ require 'rails_helper'
 
 describe "DELETE /api/v1/users/:user_id", :type => :request do
   before do
-    test_user = User.create(email: "test01@test.com", password: "123456", password_confirmation: "123456")
+    test_user = User.create(email: "testaccount_01@test.com", password: "123456", password_confirmation: "123456")
     jwt = test_user.generate_jwt
     delete "/api/v1/users/#{test_user.id}", headers: { 'Authorization': "Token #{jwt}" }
   end
